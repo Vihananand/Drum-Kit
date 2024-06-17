c4874be6e13acfcc38ccaf3a5a1545b67d597ac0
Vihan Anand <63580200+Vihananand@users.noreply.github.com>
2024-06-17 20:25:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
+    
+    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
+        
+        var buttonListner = this.innerHTML;
+        
+        switch(buttonListner){
+            case 'w':
+                var audio = new Audio('./Assets/Sounds/tom-1.mp3');
+                audio.play();
+                break;
+            
+            case 'a':
+                var audio = new Audio('./Assets/Sounds/tom-2.mp3');
+                audio.play();
+                break;
+
+            case 's':
+                var audio = new Audio('./Assets/Sounds/tom-3.mp3');
+                audio.play();
+                break;
+
+            case 'd':
+                var audio = new Audio('./Assets/Sounds/tom-4.mp3');
+                audio.play();
+                break;
+            
+            case 'j':
+                var audio = new Audio('./Assets/Sounds/snare.mp3');
+                audio.play();
+                break;
+
+            case 'k':
+                var audio = new Audio('./Assets/Sounds/crash.mp3');
+                audio.play();
+                break;
+
+            case 'l':
+                var audio = new Audio('./Assets/Sounds/kick-bass.mp3');
+                audio.play();
+                break;
+        }
+
+    })
+
+    document.querySelectorAll(".drum")[i].addEventListener("keydown" , function(event){
+
+        var key = event.key;
+        
+        switch(key){
+            case 'w':
+                var audio = new Audio('./Assets/Sounds/tom-1.mp3');
+                audio.play();
+                break;
+            
+            case 'a':
+                var audio = new Audio('./Assets/Sounds/tom-2.mp3');
+                audio.play();
+                break;
+
+            case 's':
+                var audio = new Audio('./Assets/Sounds/tom-3.mp3');
+                audio.play();
+                break;
+
+            case 'd':
+                var audio = new Audio('./Assets/Sounds/tom-4.mp3');
+                audio.play();
+                break;
+            
+            case 'j':
+                var audio = new Audio('./Assets/Sounds/snare.mp3');
+                audio.play();
+                break;
+
+            case 'k':
+                var audio = new Audio('./Assets/Sounds/crash.mp3');
+                audio.play();
+                break;
+
+            case 'l':
+                var audio = new Audio('./Assets/Sounds/kick-bass.mp3');
+                audio.play();
+                break;
+        }
+
+
+    })
+}
