618fa606879fb27b55e2bc2b52d5efeff8831288
Vihan Anand <63580200+Vihananand@users.noreply.github.com>
2024-06-17 20:34:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,10 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
         }
 
     })
+    
+}
 
+for(var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("keydown" , function(event){
 
         var key = event.key;
